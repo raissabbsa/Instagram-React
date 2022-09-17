3d105d82a883b3e805da6aaa31992630e7d5b9fa
Raissa Barbosa <raissabbsa@gmail.com>
2022-09-17 18:17:03 -0300

    versao com requisitos entregues

--- src/Posts.js
+++ src/Posts.js
@@ -1,4 +1,25 @@
+import React from "react";
+
 function RenderPost(props) {
+
+  const [salvar, setSalvar] = React.useState("bookmark-outline")
+  const [coracao, setCoracao] = React.useState("heart-outline")
+  const [classeCoracao, setClasse] = React.useState("escondido")
+  const [like, setLike] = React.useState(props.qtdd)
+
+  function curtida() {
+    if(coracao =="heart-outline") {
+      setCoracao("heart")
+      setLike(like+1)
+      setClasse("visivel")
+    }
+    else {
+      setCoracao("heart-outline")
+      setLike(like-1)
+      setClasse("escondido")
+    }
+  }
+
   return (
     <div class="post">
       <div class="topo">
@@ -18,19 +39,19 @@ function RenderPost(props) {
       <div class="fundo">
         <div class="acoes">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon onClick={curtida} name={coracao} class={classeCoracao}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon onClick={() => setSalvar("bookmark")} name={salvar}></ion-icon>
           </div>
         </div>
 
         <div class="curtidas">
           <img src={`assets/img/${props.userCurtida}.svg`} />
           <div class="texto">
-            Curtido por <strong>{props.userCurtida}</strong> e <strong>outras {props.qtdd} pessoas</strong>
+            Curtido por <strong>{props.userCurtida}</strong> e <strong>outras {like} pessoas</strong>
           </div>
         </div>
       </div>
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,14 +1,21 @@
+import React from "react";
+
 export default function Usuario() {
-    return (
-        <div class="usuario">
-          <img src="assets/img/catanacomics.svg" />
-          <div class="texto">
-            <strong>catanacomics</strong>
-            <span>
-              Catana
-              <ion-icon name="pencil"></ion-icon>
-            </span>
-            </div>
-        </div>
-    );
+
+  const [nome, setNome] = React.useState("Catana")
+  const [foto, setFoto] = React.useState("assets/img/catanacomics.svg")
+
+  return (
+    <div class="usuario">
+      <img onClick = {() => setFoto(prompt("Insira sua foto"))} src={foto} />
+      <div class="texto">
+        <strong>catanacomics</strong>
+        <span>
+          {nome}
+          <ion-icon onClick={() => setNome(prompt("Insira o seu nome"))} name="pencil"></ion-icon>
+        </span>
+      </div>
+    </div>
+  );
 }
+
